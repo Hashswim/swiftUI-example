0e5657aade55b269535094735f4d6cc9efbb10b3
hashswim <ss991117@naver.com>
2023-11-05 13:43:00 +0900

    Initial Commit

---
+++ swiftUI-example/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  swiftUI-example
+//
+//  Created by 서수영 on 2023/11/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var mystack: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+    var body: some View {
+        HStack {
+            mystack
+                .padding()
+            mystack
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI-example/swiftUI_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_exampleApp.swift
+//  swiftUI-example
+//
+//  Created by 서수영 on 2023/11/05.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
